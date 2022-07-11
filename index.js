96d082db88e48ebaff39f0c37ebf5a7011f616c2
NataliyaNikulshina <victory515@yandex.ru>
2022-07-11 23:53:30 +0300

    feat: реализация лайков

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let editButton = document.querySelector('.profile__edit-button');
-let editPopup = document.querySelector('.popup_function_edit');
-let addPopup = document.querySelector('.popup_function_add');
+let editPopup = document.querySelector('.popup_type_edit');
+let addPopup = document.querySelector('.popup_type_add');
 let closeButton = document.querySelector('.popup__button-close');
 let addButton = document.querySelector('.profile__add-button');
-
+//открыть формы
 function openedEditPopup(){
     editPopup.classList.add('popup_opened');
 }
@@ -22,31 +22,32 @@ addButton.addEventListener('click', openedAddPopup);
 
 
 
-
+//редактировать профиль 
 let saveButton = document.querySelector('.popup__button-save');
 
 function editInfo(){
 const formElement = document.querySelector('.popup__container');
 const nameInput = document.querySelector('.popup__item_name');
 const jobIntut = document.querySelector('.popup__item_about-me');
-
 function formSubmitHandler(evt){
     evt.preventDefault();
     let userName = document.querySelector('.profile__nickname');
     let userAboutMe = document.querySelector('.profile__about-me');
     userName.textContent = nameInput.value;
     userAboutMe.textContent = jobIntut.value;
-    nameInput.value = '';
-    jobIntut.value = '';
 }
-
 formElement.addEventListener('submit', formSubmitHandler);
 closedPopup();
+console.log(nameInput.value);
+console.log(jobIntut.value);
 }
 
 saveButton.addEventListener('click', editInfo);
 
-console.log(saveButton);
-
-console.log(nameInput);
-console.log(jobIntut);
+//лайк карточки
+let likeButton = document.querySelectorAll('.element__like');
+function likeActive(){
+    let likeElement = document.querySelector('.element__like');
+    likeElement.classList.toggle('element__like_active');
+}
+likeButton.addEventListener('click', likeActive);
