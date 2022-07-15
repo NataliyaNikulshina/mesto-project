aa71bc8b7768c3d6a44e444ddb8bc3749ba98fe0
NataliyaNikulshina <victory515@yandex.ru>
2022-07-15 18:24:14 +0300

    fix: исправление ошибок 2.0

--- pages/index.js
+++ pages/index.js
@@ -12,27 +12,27 @@ const saveElementsButton = document.querySelector('.popup__button-save_cards');
 const initialCards = [
     {
       name: 'Плато Матлас',
-      link: './images/elements/plato_matlas.jpg'
+      link: '../images/elements/plato_matlas.jpg'
     },
     {
       name: 'Гора Ахун',
-      link: './images/elements/mountain_ahyn.jpg'
+      link: '../images/elements/mountain_ahyn.jpg'
     },
     {
       name: 'Куршская коса',
-      link: './images/elements/kyrshskaya_kosa.jpg'
+      link: '../images/elements/kyrshskaya_kosa.jpg'
     },
     {
       name: 'Карелия',
-      link: './images/elements/karelia.jpg'
+      link: '../images/elements/karelia.jpg'
     },
     {
       name: 'Крым',
-      link: './images/elements/krym.jpg'
+      link: '../images/elements/krym.jpg'
     },
     {
       name: 'Дагестан',
-      link: './images/elements/dagestan.jpg'
+      link: '../images/elements/dagestan.jpg'
     }
   ]; 
 
