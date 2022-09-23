eddfa8c121a35da67d8128791c1f3f0e4eb228da
NataliyaNikulshina <victory515@yandex.ru>
2022-09-24 02:07:00 +0300

    hotfix: added param in validation

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,4 +1,12 @@
-import {objValidation} from '../pages/index.js'; 
+const objValidation = ({
+  formSelector: '.popup__container',
+  fieldsetSelector: '.popup__user-info',
+  inputSelector: '.popup__item',
+  submitButtonSelector: '.popup__button-save',
+  inactiveButtonClass: 'popup__button-save_inactive',
+  inputErrorClass: 'popup__item_type_error',
+  errorClass: 'popup__input-error_active'
+}); 
 
 const hasInvalidInput = (inputList) => {
     return inputList.some((inputElement) => {
