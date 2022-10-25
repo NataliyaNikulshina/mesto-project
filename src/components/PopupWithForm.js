561910cab842fa711482aee8625a571d3f6da306
Yulia Selivanova <selivanyulia@mail.ru>
2022-10-25 23:44:55 +0300

    fix: change close method of PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,18 +1,16 @@
 import Popup from "../components/Popup.js";
 
-
-const forms = document.querySelectorAll('.popup__container')
-//const inputs = document.querySelectorAll('.popup__item')
-
 export default class PopupWithForm extends Popup {
   constructor(popupSelector, submitForm) {
     super(popupSelector);
     this._submitForm = submitForm;
     this._popup = document.querySelector(popupSelector);
+    this._form = this._popup.querySelector('.popup__container')
   }
 
  /* _getInputValues() {
     inputs.forEach((input) => {
+      const inputs = document.querySelectorAll('.popup__item')
       console.log(input.value)
       return input.value
     })
@@ -21,13 +19,10 @@ export default class PopupWithForm extends Popup {
   setEventListeners() {
     super.setEventListeners();
     this._popup.addEventListener("submit", this._submitForm);
-
   }
 
   close() {
     super.close();
-    forms.forEach((form) => {
-      form.reset();
-    })
+    this._form.reset();
   }
 }
